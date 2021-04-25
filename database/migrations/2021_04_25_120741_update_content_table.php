36f3be704cd5f07364a262e65d47161afa33eb0b
rishabgoyal368 <rishabgoyal368@gmail.com>
2021-04-25 18:02:38 +0530

    null data type

---
+++ database/migrations/2021_04_25_120741_update_content_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateContentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('content', function (Blueprint $table) {
+            $table->string('audio')->nullable()->change();
+            $table->string('image')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('content', function (Blueprint $table) {
+            //
+        });
+    }
+}
